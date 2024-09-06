a6ebe0bcb6e72cf8d81ff97764d873cc2963940f
Abdul Hakim <hakim@undercurrent.tech>
2024-09-06 10:54:58 +0700

    fixed lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,61 @@
-"use client";
+'use client'
 
-import useMediaQuery from '@mui/material/useMediaQuery';
+import useMediaQuery from '@mui/material/useMediaQuery'
 
-import { NearContext } from "@/src/contexts/NearContext";
-import { useEffect, useState } from "react";
-import { useTheme, createTheme } from '@mui/material/styles';
-import { ThemeProvider } from "@emotion/react";
-import { BurnContract, NetworkId } from '@/src/config'
-import { Wallet } from "@/src/near";
-import { ContractProvider } from "@/src/contexts/ContractContext";
-import { Chat } from '@/src/components/Chat';
-import { SnackbarProvider } from 'notistack';
-import { CssBaseline } from '@mui/material';
+import { NearContext } from '@/src/contexts/NearContext'
+import { useEffect, useState } from 'react'
+import { useTheme, createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@emotion/react'
+import { Wallet } from '@/src/near'
+import { ContractProvider } from '@/src/contexts/ContractContext'
+import { Chat } from '@/src/components/Chat'
+import { SnackbarProvider } from 'notistack'
+import { CssBaseline } from '@mui/material'
+import { AppConfig } from '@/src/config'
+import { NetworkId } from '@near-wallet-selector/core'
 
 const wallet = new Wallet({
-  createAccessKeyFor: BurnContract,
-  networkId: NetworkId,
+  createAccessKeyFor: AppConfig.CONTRACT.BURN,
+  networkId: AppConfig.NETWORK as NetworkId,
   methodNames: ['converse']
 })
 
 let themes = createTheme({
   palette: {
     primary: {
-      main : '#FF56E0'
+      main: '#FF56E0'
     },
     secondary: {
-      main : '#FEFEFE'
+      main: '#FEFEFE'
     },
     background: {
       default: '#363636'
-    },
-  },
-});
+    }
+  }
+})
 
 export default function Home() {
-  const [signedAccountId, setSignedAccountId] = useState('');
+  const [signedAccountId, setSignedAccountId] = useState('')
 
   useEffect(() => {
     wallet.startUp(setSignedAccountId)
   }, [])
 
   const theme = useTheme()
-  const isMobile = !useMediaQuery(theme.breakpoints.up('md'));
+  const isMobile = !useMediaQuery(theme.breakpoints.up('md'))
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <SnackbarProvider>
         <NearContext.Provider value={{ wallet, signedAccountId }}>
           <ContractProvider>
             <ThemeProvider theme={themes}>
-              <CssBaseline/>
-              <Chat isMobile={isMobile}/>
+              <CssBaseline />
+              <Chat isMobile={isMobile} />
             </ThemeProvider>
           </ContractProvider>
         </NearContext.Provider>
       </SnackbarProvider>
     </main>
-  );
+  )
 }
